5701099d501853081e4448477d1a0b222f6f0436
Piao Pamela <pamela.piao@sap.com>
2019-09-24 02:18:33 +0000

    bind bookshop service broker as main service

--- app/webapp/controller/View1.controller.js
+++ app/webapp/controller/View1.controller.js
@@ -1,10 +1,29 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"test/sap/app/controller/BaseController"
+], function (BaseController) {
 	"use strict";
 
-	return Controller.extend("test.sap.app.controller.View1", {
+	return BaseController.extend("test.sap.app.controller.View1", {
 		onInit: function () {
+			var oViewModel,
+				iOriginalBusyDelay,
+				oTable = this.byId("table");
+
+			// Put down worklist table's original value for busy indicator delay,
+			// so it can be restored later on. Busy handling on the table is
+			// taken care of by the table itself.
+			iOriginalBusyDelay = oTable.getBusyIndicatorDelay();
+			this._oTable = oTable;
+			// keeps the search state
+			this._oTableSearchState = [];
+
+			// Make sure, busy indication is showing immediately so there is no
+			// break after the busy indication for loading the view's meta data is
+			// ended (see promise 'oWhenMetadataIsLoaded' in AppController)
+			oTable.attachEventOnce("updateFinished", function () {
+				// Restore original busy indicator delay for worklist's table
+				oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
+			});
 
 		}
 	});
